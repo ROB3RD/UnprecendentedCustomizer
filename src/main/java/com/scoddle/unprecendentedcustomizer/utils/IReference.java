32f010c8308bd8491c2d41e13365d0cf64f45d69
CorruptionHades <corruptionhades@gmail.com>
2022-07-23 10:38:10 +0200

    + IReference

---
+++ src/main/java/com/scoddle/unprecendentedcustomizer/utils/IReference.java
@@ -0,0 +1,8 @@
+package com.scoddle.unprecendentedcustomizer.utils;
+
+import com.scoddle.unprecendentedcustomizer.UnprecendentedCustomizer;
+
+public interface IReference {
+
+    UnprecendentedCustomizer plugin = UnprecendentedCustomizer.getPlugin(UnprecendentedCustomizer.class);
+}
